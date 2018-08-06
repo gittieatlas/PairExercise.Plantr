eeaf6ab662f58849bc3d7843997f05f479a6cfdc
Gittie Atlas <gittieatlas@gmail.com>
2018-08-06 19:29:54 -0400

    comitting Adils round of code

--- models.js
+++ models.js
@@ -27,4 +27,4 @@ Plot.belongsToMany(Vegetable, { through: 'veg_plot' });
 
 Gardener.belongsTo(Vegetable, { as: 'favorite_vegetable' });
 
-module.exports = db;
+module.exports = { db, Gardener, Plot, Vegetable };
--- seed.js
+++ seed.js
@@ -1,16 +1,51 @@
-const db = require('./models');
+const { db, Gardener, Plot, Vegetable } = require('./models');
 
-db.sync({force: true})
-.then(() => {
-  console.log('Database synced!');
-  // db.close() // only if using a version of node without `finally`
-})
-.catch(err => {
-  console.log('Disaster! Something went wrong! ');
-  console.log(err);
-  // db.close() // only if using a version of node without `finally`
-})
-.finally(() => { // only if using a version of node WITH `finally`
-console.log('Database closed');
-  db.close();
-});
+db.sync({ force: true })
+  .then(() => {
+    console.log('Database synced!');
+    //one return for 5 veggies
+    //promise.all
+    const carrotP = Vegetable.create({
+      name: 'Carrot',
+      color: 'purple',
+      planted_on: Date.now()
+    });
+    const pepperP = Vegetable.create({
+      name: 'Pepper',
+      color: 'green',
+      planted_on: Date.now()
+    });
+    const kaleP = Vegetable.create({
+      name: 'Kale',
+      color: 'purple / green',
+      planted_on: Date.now()
+    });
+    const veggiesP = [carrotP, pepperP, kaleP];
+    console.log('Veggies created');
+    return Promise.all(veggiesP);
+  })
+  .then(createdVegetables => {
+    const adilP = Gardener.create({ name: 'Adil', age: 30 });
+    const gittieP = Gardener.create({ name: 'Gittie', age: 25 });
+    const tessP = Gardener.create({ name: 'Tess', age: 28 });
+    const gardenersP = [adilP, gittieP, tessP];
+    console.log('Gardeners created');
+    return Promise.all(gardenersP);
+  })
+  .then(createdGardeners => {
+    const plot1P = Plot.create({ size: 25, shaded: false });
+    const plot2P = Plot.create({ size: 1, shaded: true });
+    const plot3P = Plot.create({ size: 100, shaded: true });
+    const plotsP = [plot1P, plot2P, plot3P];
+    console.log('Plots created');
+    return Promise.all(plotsP);
+  })
+  .catch(err => {
+    console.log('Disaster! Something went wrong! ');
+    console.log(err);
+  })
+  .finally(() => {
+    // only if using a version of node WITH `finally`
+    console.log('Database closed!');
+    db.close();
+  });
