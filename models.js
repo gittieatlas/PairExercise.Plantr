a40f9c132c441db5ec9639d18a6e40c26881e2f3
Gittie Atlas <gittieatlas@gmail.com>
2018-08-06 17:20:12 -0400

    created models

--- models.js
+++ models.js
@@ -1,3 +1,30 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost:5432/plantr');
+const db = new Sequelize('postgres://localhost:5432/plantr', {
+  logging: false
+});
+
+const Gardener = db.define('gardener', {
+  name: Sequelize.STRING,
+  age: Sequelize.INTEGER
+});
+
+const Plot = db.define('plot', {
+  size: Sequelize.INTEGER,
+  shaded: Sequelize.BOOLEAN
+});
+
+const Vegetable = db.define('vegetable', {
+  name: Sequelize.STRING,
+  color: Sequelize.STRING,
+  planted_on: Sequelize.DATE
+});
+
+Plot.belongsTo(Gardener);
+Gardener.hasOne(Plot);
+
+Vegetable.belongsToMany(Plot, { through: 'veg_plot' });
+Plot.belongsToMany(Vegetable, { through: 'veg_plot' });
+
+Gardener.belongsTo(Vegetable, { as: 'favorite_vegetable' });
+
 module.exports = db;
